1c5642d688ad8be9869cdcb5b8497d477ac8b13c
ariyansalari <ariyan4509@gmail.com>
2024-03-13 16:19:37 -0700

    feat(page): implement-playMusic

--- src/app/(site)/components/PageContent/PageContent.tsx
+++ src/app/(site)/components/PageContent/PageContent.tsx
@@ -2,11 +2,13 @@
 
 import { Song } from "@/types"
 import { SongItem } from "./partials"
+import { useOnPlay } from "@/hooks"
 
 interface PageContentProps {
     songs:Song[]
 }
 export const PageContent:React.FC<PageContentProps> = ({songs}) => {
+  const onPlay=useOnPlay(songs)
     if(songs.length===0){
         return (
             <div className="mt-4 text-neutral-400">
@@ -17,7 +19,7 @@ No songs available.
   return (
     <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-8 gap-4 mt-4">
     {songs?.map((item) => (
-      <SongItem key={item.id} onClick={()=>{}} data={item}/>
+      <SongItem key={item.id} onClick={(id:string)=>onPlay(id)} data={item}/>
     ))}
   </div>
   )
--- src/ui/components/navigation/index.ts
+++ src/ui/components/navigation/index.ts
@@ -1,3 +1,5 @@
 export * from "./Modal"
 export * from "./AuthModal"
 export * from "./UploadModal"
+export * from "./PlayerBottom"
+export * from "./Slider"
